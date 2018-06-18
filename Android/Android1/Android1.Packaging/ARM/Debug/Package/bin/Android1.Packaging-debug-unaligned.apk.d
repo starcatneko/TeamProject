8c5ebc0237e1fd166439ffc445c7253e11c610c5
okmonn <35838332+okmonn@users.noreply.github.com>
2018-06-18 14:25:21 +0900

    ｄｆｇｈ

--- Android/Android1/Android1.Packaging/ARM/Debug/Package/bin/Android1.Packaging-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-C:\Users\1601271\Documents\GitHub\TeamProject\Android\Android1\Android1.Packaging\ARM\Debug\Package\bin\Android1.Packaging-debug-unaligned.apk : \
-C:\Users\1601271\Documents\GitHub\TeamProject\Android\Android1\Android1.Packaging\ARM\Debug\Package\bin\Android1.Packaging.ap_ \
-C:\Users\1601271\Documents\GitHub\TeamProject\Android\Android1\Android1.Packaging\ARM\Debug\Package\libs\armeabi-v7a\gdbserver \
-C:\Users\1601271\Documents\GitHub\TeamProject\Android\Android1\Android1.Packaging\ARM\Debug\Package\libs\armeabi-v7a\libAndroid1.so \
--- Android/Android1/Android1.Packaging/ARM/Debug/Package/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\1601271\Documents\GitHub\TeamProject\Android\Android1\Android1.Packaging\ARM\Debug\Package\bin\AndroidManifest.xml : \
-C:\Users\1601271\Documents\GitHub\TeamProject\Android\Android1\Android1.Packaging\ARM\Debug\Package\AndroidManifest.xml \
