7c565572b2f993b72bf703d7da33c5100ff81cc5
starcatneko <star.cat.neko@gmail.com>
2018-06-22 14:15:33 +0900

    さうんど更新

--- Android/Android1/Android1.NativeActivity/Sound.cpp
+++ Android/Android1/Android1.NativeActivity/Sound.cpp
@@ -45,14 +45,14 @@ void Sound::Load(std::string fileName, SOUND type)
 // 読み込み
 void Sound::SoundInit(void)
 {
-	Load("hit1.mp3", SE_HIT1);
-	Load("hit2.mp3", SE_HIT2);
-	Load("voice1.mp3", SE_VOICE1);
-	Load("voice2.mp3", SE_VOICE2);
-	Load("ko.mp3", SE_KO);
-	Load("dead.mp3", SE_DEAD);
-	Load("step.mp3", SE_STEP);
-	Load("heal.mp3", SE_HEAL);
+	Load("hit1.ogg", SE_HIT1);
+	Load("hit2.ogg", SE_HIT2);
+	Load("voice1.ogg", SE_VOICE1);
+	Load("voice2.ogg", SE_VOICE2);
+	Load("ko.ogg", SE_KO);
+	Load("dead.ogg", SE_DEAD);
+	Load("step.ogg", SE_STEP);
+	Load("heal.ogg", SE_HEAL);
 }
 
 // サウンドの再生
